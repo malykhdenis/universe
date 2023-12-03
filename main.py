d30b97b5cbd48b87574d46b88a7d4634000b6633
malykh <malykhdenis@gmail.com>
2023-12-03 12:24:33 +0300

    def download_nasa_images() added

--- main.py
+++ main.py
@@ -43,6 +43,25 @@ def get_format(url):
 	return file_format
 
 
+def download_nasa_images():
+	"""Download photos from NASA."""
+	Path('images/nasa').mkdir(parents=True, exist_ok=True)
+	payload = {
+		'api_key': os.getenv('NASA_TOKEN'),
+		'count': 30,
+	}
+	response_nasa = requests.get(
+		'https://api.nasa.gov/planetary/apod',
+		params=payload
+	)
+	response_nasa.raise_for_status()
+	photos = response_nasa.json()
+	photo_number = 0
+	for photo in photos:
+		with open(f'images/nasa_apod_{photo_number}.jpeg', 'wb') as file:
+  			file.write(requests.get(photo['url']).content)
+		photo_number += 1
+
 
 if __name__ == '__main__':
-	print(get_format('https://example.com/txt/hello%20world.txt?v=9#python'))
+	download_nasa_images()
