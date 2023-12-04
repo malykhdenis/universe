1cbeedc9f4fd7ffffe4ca42dec922b3c9aa96d84
Денис Малых <malykhdenis@gmail.com>
2023-12-04 23:59:17 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -9,4 +9,11 @@ bot = telegram.Bot(token=os.getenv('TELEGRAM_TOKEN'))
 
 print(bot.get_me())
 
-bot.send_message(text='Hi John!', chat_id=-1002127154538)
+channel_id = -1002127154538
+
+bot.send_message(chat_id=channel_id, text='This is NASA!')
+
+bot.send_photo(
+    chat_id=channel_id,
+    photo='https://apod.nasa.gov/apod/image/2107/LRVBPIX3M82Crop1024.jpg',
+)
